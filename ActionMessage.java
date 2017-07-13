e8361845c90aaead2ec87d6aa30b7fa2d4d94f2d
Forest Wcl <cl_wu01@163.com>
2017-07-13 19:38:30 +0800

    Create ActionMessage.java
    servlet

---
+++ ActionMessage.java
@@ -0,0 +1,29 @@
+package com.wuchanglin.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/manage/messagesend")
+public class ActionMessage extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String message = req.getParameter("message");
+		//System.out.println(message);
+		WebSocketHandler.sendDataToAllSession("【通知】："+ message);
+		req.setAttribute("tip", "发送成功！");
+		req.getRequestDispatcher("/send.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp);
+	}
+}
